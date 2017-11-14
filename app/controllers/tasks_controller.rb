9522faba6dcead3d87155ff050c193b50ac9673a
Luciano Oliveira <luciano101@msn.com>
2017-11-13 20:13:09 -0800

    update and delete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,30 @@ class TasksController < ApplicationController
     end
   end
 
+   def edit  
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to list_path(@task.list)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@task.list)
+  end
+
+
 private
   def task_params
     params.require(:task).permit(:description)
